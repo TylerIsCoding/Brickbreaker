c1fb87fd67cca22156e12a19544008818ba7380b
yeoldeknob <tmckenna1988@gmail.com>
2022-04-14 15:53:47 -0400

    4/14 Update
    Still working on the ball collision with the paddle. This is a start.

--- src/ball.js
+++ src/ball.js
@@ -7,6 +7,7 @@ export default class Ball {
         };
         this.position = { x: 10, y: 10 };
         this.size = 16;
+        this.game = game;
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
     }
@@ -23,15 +24,27 @@ export default class Ball {
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;
 
+        // Does ball hit the left or right side?
         if (
             this.position.x + this.size > this.gameWidth ||
             this.position.x < 0
         ) {
             this.speed.x = -this.speed.x;
         }
+
+        // Does ball hit the top or bottom?
         if (
             this.position.y + this.size > this.gameHeight ||
             this.position.y < 0
+        ) {
+            this.speed.y = -this.speed.y;
+        }
+
+        // Does ball hit the paddle?
+        if (
+            this.position.y + this.game.paddle.height ===
+                this.game.paddle.position.y &&
+            this.position.x === this.game.paddle.position.x
         ) {
             this.speed.y = -this.speed.y;
         }
