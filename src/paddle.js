b50a145d54c80632f07e7be408f04bbed189bfb1
yeoldeknob <tmckenna1988@gmail.com>
2022-04-14 11:33:04 -0400

    4/14 Update
    Movement added to 'paddle' object

--- src/paddle.js
+++ src/paddle.js
@@ -1,8 +1,10 @@
 export default class Paddle {
     constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
         this.width = 150;
         this.height = 20;
-
+        this.maxSpeed = 7;
+        this.speed = 0;
         this.position = {
             x: gameWidth / 2 - this.width / 2,
             y: gameHeight - this.height - 10,
@@ -16,13 +18,21 @@ export default class Paddle {
 
     update(deltaTime) {
         if (!deltaTime) return;
+        this.position.x += this.speed;
+
+        if (this.position.x < 0) {
+            this.position.x = 0;
+        }
+        if (this.position.x + this.width > this.gameWidth) {
+            this.position.x = this.gameWidth - this.width;
+        }
     }
 
     moveLeft() {
-        this.position.x -= 10;
+        this.speed = -this.maxSpeed;
     }
 
     moveRight() {
-        this.position.x += 10;
+        this.speed = this.maxSpeed;
     }
 }
