f10ae15f53150283011943e10f38139372803358
yeoldeknob <tmckenna1988@gmail.com>
2022-04-15 14:01:08 -0400

    4/15 Update
    Game over screen added.
    Lives added (3 by default).
    New level added.

--- src/ball.js
+++ src/ball.js
@@ -3,16 +3,21 @@ import { detectCollision } from '/src/collisionDetection.js';
 export default class Ball {
     constructor(game) {
         this.image = document.getElementById('img_ball');
+        this.size = 16;
+        this.game = game;
+        this.gameWidth = game.gameWidth;
+        this.gameHeight = game.gameHeight;
+        this.reset();
+    }
+
+    reset() {
         this.speed = {
             x: 6,
             y: -6,
         };
         this.position = { x: 10, y: 300 };
-        this.size = 16;
-        this.game = game;
-        this.gameWidth = game.gameWidth;
-        this.gameHeight = game.gameHeight;
     }
+
     draw(ctx) {
         ctx.drawImage(
             this.image,
@@ -33,12 +38,14 @@ export default class Ball {
             this.speed.x = -this.speed.x;
         }
 
-        // Does ball hit the top or bottom?
-        if (
-            this.position.y + this.size > this.gameHeight ||
-            this.position.y < 0
-        ) {
+        // Does ball hit the top?
+        if (this.position.y < 0) {
             this.speed.y = -this.speed.y;
+        }
+
+        if (this.position.y + this.size > this.gameHeight) {
+            this.game.lives--;
+            this.reset();
         }
 
         if (detectCollision(this, this.game.paddle)) {
--- src/game.js
+++ src/game.js
@@ -1,13 +1,14 @@
 import Paddle from '/src/paddle.js';
 import InputHandler from '/src/input.js';
 import Ball from '/src/ball.js';
-import { buildLevel, level1 } from '/src/levels.js';
+import { buildLevel, level1, level2 } from '/src/levels.js';
 
 const GAMESTATE = {
     PAUSED: 0,
     RUNNING: 1,
     MENU: 2,
     GAMEOVER: 3,
+    NEWLEVEL: 4,
 };
 
 export default class Game {
@@ -17,29 +18,50 @@ export default class Game {
         this.gamestate = GAMESTATE.MENU;
         this.paddle = new Paddle(this);
         this.ball = new Ball(this);
+        this.lives = 3;
         this.gameObjects = [];
+        this.bricks = [];
+        this.levels = [level1, level2];
+        this.currentLevel = 0;
         new InputHandler(this.paddle, this);
     }
     start() {
-        if (this.gamestate !== GAMESTATE.MENU) return;
-        let bricks = buildLevel(this, level1);
-        this.gameObjects = [this.ball, this.paddle, ...bricks];
+        if (
+            this.gamestate !== GAMESTATE.MENU &&
+            this.gamestate !== GAMESTATE.NEWLEVEL
+        )
+            return;
+        this.bricks = buildLevel(this, this.levels[this.currentLevel]);
+        this.ball.reset();
+        this.gameObjects = [this.ball, this.paddle];
         this.gamestate = GAMESTATE.RUNNING;
     }
     update(deltaTime) {
+        if (this.lives === 0) {
+            this.gamestate = GAMESTATE.GAMEOVER;
+        }
         if (
             this.gamestate === GAMESTATE.PAUSED ||
-            this.gamestate === GAMESTATE.MENU
+            this.gamestate === GAMESTATE.MENU ||
+            this.gamestate === GAMESTATE.GAMEOVER
         )
             return;
-        this.gameObjects.forEach(object => object.update(deltaTime));
 
-        this.gameObjects = this.gameObjects.filter(
-            object => !object.markedForDeletion
+        if (this.bricks.length === 0) {
+            this.currentLevel++;
+            this.gamestate = GAMESTATE.NEWLEVEL;
+            this.start();
+        }
+        [...this.gameObjects, ...this.bricks].forEach(object =>
+            object.update(deltaTime)
         );
+
+        this.bricks = this.bricks.filter(object => !object.markedForDeletion);
     }
     draw(ctx) {
-        this.gameObjects.forEach(object => object.draw(ctx));
+        [...this.gameObjects, ...this.bricks].forEach(object =>
+            object.draw(ctx)
+        );
         if (this.gamestate === GAMESTATE.PAUSED) {
             ctx.rect(0, 0, this.gameWidth, this.gameHeight);
             ctx.fillStyle = 'rgba(0,0,0,0.5)';
@@ -62,6 +84,16 @@ export default class Game {
                 this.gameWidth / 2,
                 this.gameHeight / 2
             );
+        }
+
+        if (this.gamestate === GAMESTATE.GAMEOVER) {
+            ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+            ctx.fillStyle = 'rgba(0,0,0,1)';
+            ctx.fill();
+            ctx.font = '30px Arial';
+            ctx.fillStyle = 'white';
+            ctx.textAlign = 'center';
+            ctx.fillText('GAME OVER', this.gameWidth / 2, this.gameHeight / 2);
         }
     }
     togglePause() {
